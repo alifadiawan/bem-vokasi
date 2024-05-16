848e4150985b6f53b779ef029e6fee3f21d0601a
alif <alifadiawan2005@gmail.com>
2024-05-16 11:46:49 +0700

    fix: bg yang ilang2; new: bg in halaman profile, logo vokasi di home

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,7 +4,7 @@
  <!-- Hero Section -->
  <section id="hero" class="hero section">
 
-    <img id="background" src="{{ asset('sailor/img/bg-unair.jpg') }}" alt="" data-aos="fade-down"
+    <img id="background" src="{{ asset('ppp/img/profile-bg.webp') }}" alt="" data-aos="fade-down"
         data-aos-duration="1000">
 
     <div class="container">
@@ -15,12 +15,12 @@
         </div>
         <div class="row text-center justify-content-center align-items-center floating">
             <div class="col">
-                <img src="{{ asset('assets/img/logo_bem.png') }}" alt="" data-aos="fade-left"
+                <img src="{{ asset('ppp/img/logo_bem.png') }}" alt="" data-aos="fade-left"
                 data-aos-duration="1500" data-aos-delay="1200" class="img-fluid floating" style="height: 15rem">
                 <h4>Ketua</h4>
             </div>
             <div class="col">
-                <img src="{{ asset('assets/img/logo_bem.png') }}" alt="" data-aos="fade-left"
+                <img src="{{ asset('ppp/img/logo_bem.png') }}" alt="" data-aos="fade-left"
                     data-aos-duration="1500" data-aos-delay="1200" class="img-fluid floating" style="height: 15rem">
                 <h4>Wakil Ketua</h4>
             </div>
