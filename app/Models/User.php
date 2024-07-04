c19128e68311692db95bd9079b7cc7e3848dbb0e
alif <alifadiawan2005@gmail.com>
2024-07-05 00:37:42 +0700

    new: blog crud done with filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Firefly\FilamentBlog\Traits\HasBlog;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasBlog;
 
     /**
      * The attributes that are mass assignable.
@@ -43,5 +45,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function canComment(): bool
+    {
+        // your conditional logic here
+        return true;
     }
 }
